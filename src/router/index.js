82e5d76681d5f834e2ca0be9a9d1ab982d89f913
iYzee23 <predrag.pesic17@gmail.com>
2023-06-29 22:00:02 +0200

    Treci commit.

--- src/router/index.js
+++ src/router/index.js
@@ -30,14 +30,6 @@ const routes = [
     path: '/umetnik/:id',
     name: 'Umetnik',
     component: UmetnikView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 

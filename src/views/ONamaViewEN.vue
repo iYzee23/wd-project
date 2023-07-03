b002edc041b1085a591708d6f01dea94ec6acd12
nl200370 <nl200370d@student.etf.bg.ac.rs>
2023-07-03 17:56:36 +0200

    Nevajda-dodate engleske verzije sajtova

---
+++ src/views/ONamaViewEN.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <br>
+                <h2>About us</h2>
+                <p> Welcome to LiP, the online art gallery that celebrates creativity and connects artists with art enthusiasts from around the world. At LiP, we strive to showcase a diverse range of artwork across various mediums, including paintings, sculptures, photography, and more. Our platform provides a unique space for talented artists to exhibit their work and for art lovers to discover and collect exceptional pieces. Whether you're an artist looking for exposure or an art enthusiast seeking inspiration, LiP is your gateway to an immersive art experience. Join us on this artistic journey and explore the captivating world of LiP.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <h4>Where to find us?</h4>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2830.7962936869494!2d20.47532344378023!3d44.805339951315155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a7a9f5ee145d3%3A0x3ed89b5bb505d83!2sUniversity%20of%20Belgrade%20School%20of%20Electrical%20Engineering!5e0!3m2!1sen!2srs!4v1688286125877!5m2!1sen!2srs" width="70%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> 
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <br>           
+                <h4>
+                    Contact telephone: +381/11 1234-321
+                </h4>
+                <br>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .container-fluid{
+        text-align: center;
+        border: 5px solid #C4A484;
+    }
+    .col-sm-12 h2{
+        color:plum;
+    }
+    .footer{
+        color:plum;
+        font-size: 10px;
+    }
+</style>
+
---
+++ src/views/UmetniciViewEN.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div v-for="artist in artists" :key="artist.artist_id" class="col-sm-12 col-md-6 col-lg-3">
+        <div class="card sm-12 md-6 lg-3" style="max-width: 500px;">
+            <div class="row">
+                <div class="col-12 col-sm-4">
+                    <img :src="'/images/artist'+artist.artist_id+'.jpeg'" class="card-img">
+                </div>
+                <div class="col-12 col-sm-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ artist.artist_name }}</h5>
+                        <p class="card-text">{{ artist.artist_short_biography }}</p>
+                        <a :href="'/umetnik/'+artist.artist_id" class="btn btn-outline-danger">Visit artist's profile</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+    .container-fluid{
+        text-align: right;
+        border: 5px solid #C4A484;
+    }
+    .card{
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    .card-title{
+        color: plum;
+    }
+    .card-img{
+        width: 150px;
+        height: 200px;
+    }
+</style>
+
+<script>
+import allArtists from '../data/allArtists.js'
+export default {
+  data() {
+    return {
+      artists: allArtists
+    };
+  }
+};
+</script>
